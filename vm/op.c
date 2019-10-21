4ab67d9f6b9a513667f68d127250fe0d820d19c4
EtienneViana <etienne.viana@gmail.com>
2019-10-21 18:22:47 +0200

    recup op.c Etienne

--- vm/op.c
+++ vm/op.c
@@ -6,13 +6,13 @@
 /*   By: eviana <eviana@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by zaz               #+#    #+#             */
-/*   Updated: 2019/10/17 15:10:43 by eviana           ###   ########.fr       */
+/*   Updated: 2019/10/16 13:34:57 by eviana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "virtual_machine.h"
+#include "op.h"
 
-/*t_op    op_tab[17] =
+t_op    op_tab[17] =
 {
 	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 0},
 	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0},
@@ -36,5 +36,138 @@
 		"long load index", 1, 1},
 	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},
 	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0},
-	{0, 0, {0, 0, 0}, 0, 0, 0, 0, 0}
-};*/
+	{0, 0, {0}, 0, 0, 0, 0, 0}
+};
+
+int			rel_address(t_vm *vm, int proc_id, int add1, int add2)
+{
+	return ((vm->proc[proc_id]->pc + ((add1 + add2) % IDX_MOD)) % MEM_SIZE);
+}
+
+int			read_address(t_vm *vm, int pc, size_t bytes)
+{
+	int		res;
+
+	while (bytes)
+	{
+		res = vm->mem[pc] * ft_power(256, bytes - 1);
+		pc = (pc + 1) % MEM_SIZE;
+		bytes--;
+	}
+	return (res);
+}
+
+void		move_pc(t_vm *vm, int proc_id, int offset)
+{
+	vm->proc[proc_id].pc = (vm->proc[proc_id].pc + offset) % MEM_SIZE;
+}
+
+int			check_ocp(char ocp)
+{
+	if ((ocp & 1) || (ocp >> 1) & 1)
+		return (0); // OCP incorrect
+	return (1);
+}
+
+int			get_code(char ocp)
+{
+	if ((ocp & 1) && ((ocp >> 1) & 1))
+		return (IND_CODE);
+	else if ((ocp >> 1) & 1)
+		return (DIR_CODE);
+	else if (ocp & 1)
+		return (REG_CODE);
+	else
+		return (0);
+}
+
+int			get_param(t_vm *vm, int pc, int code, int dir_size)
+{
+	if (code == REG_CODE)
+		return (vm->reg[read_address(vm, pc, 1)]);
+	else if (code == DIR_CODE)
+		return (read_address(vm, pc, dir_size));
+	else if (code == IND_CODE)
+		return (read_address(vm, read_address(vm, pc, 2), 4)); // attention, cela doit etre lu par rapport au pc initial et dépend de l'addressage restreint
+	return (0);
+}
+
+int			param_size(int code, int dir_size)
+{
+	if (code == REG_CODE)
+		return (1);
+	else if (code == DIR_CODE)
+		return (dir_size);
+	else if (code == IND_CODE)
+		return (2);
+	return (0);
+}
+
+t_params	set_params(t_vm *vm, int pc, int *count, int dir_size)
+{
+	int			i;
+	int			code;
+	char		ocp;
+	t_params 	params;
+
+	i = 0;
+	ocp = vm->mem[(pc + 1) % MEM_SIZE];
+	pc = (pc + 2) % MEM_SIZE;
+	*count = 2; // 1 + 1 : on passera l'opcode puis l'ocp
+	check_ocp(ocp); // Faire une action si nul : perror ?
+	while (i < 3)
+	{
+		ocp = ocp >> 2;
+		code = get_code(ocp);
+		params.n[i] = get_param(vm, pc, code, dir_size);
+		pc = (pc + param_size(code, dir_size)) % MEM_SIZE;
+		*count = *count + param_size(code, dir_size);
+		i++;
+	}
+	return (params);
+}
+
+int		zjmp(t_vm *vm, int pc)
+{
+	//vm->time += 20; // Comment traiter la durée ?
+	//move_pc(vm, proc_id, 1);
+	if (vm->carry)
+		return (read_address(vm, (pc + 1) % MEM_SIZE, 2)); // En sortie il faudra appliquer le % MEM_SIZE, on peut le faire en utilisant move_pc
+	else
+		return (3); // 1 + 2 : on passe l'opcode, puis on passe le D2
+}
+
+/*
+**	Ne pas oublier de move_pc(vm, proc_id, 1) avant chaque fonction ==> NON
+**  pour passer l'opcode et se retrouver soit sur l'ocp soit sur le 1er argument
+**	Rajouter l'adressage restreint
+*/
+
+int			ldi(t_vm *vm, int pc)
+{
+	t_params	params;
+	int			count; // Nous permet de savoir de combien de case avancer jusqu'a la fin de l'instruction
+	
+	//vm->time += 25; // Comment traiter la durée ?
+	//move_pc(vm, proc_id, 1);
+	params = set_params(vm, pc, &count, 2);
+	vm->reg[params.n[2]] = read_address(vm, rel_address(vm, proc_id, params.n[0], params.n[1]));
+	if (vm->reg[params.n[2]] == 0)
+		vm->carry = 1;
+	else
+		vm->carry = 0;
+	return (count);
+}
+
+int			sti(t_vm *vm, int proc_id)
+{
+	t_params	params;
+
+	params = set_params(vm, proc_id);
+	vm->mem[rel_address(vm, proc_id, params.n[1], params.n[2])] = vm->reg[params.n[0]];
+	if (vm->reg[params.n[0]] == 0)
+		vm->carry = 1;
+	else
+		vm->carry = 0;
+	return (0);
+}
